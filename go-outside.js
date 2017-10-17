3e792f769ccf639ee96412d27e5382ff2f10df30
bhavBains <deepbains26@gmail.com>
2017-10-17 17:03:28 +0000

    Adds conditional(if-else) exercises

---
+++ go-outside.js
@@ -0,0 +1,14 @@
+var raining = true;
+var cold = false;
+
+if(raining){
+	console.log("Don't forget your umbrella!");
+}
+
+if(cold){
+	console.log("Make sure you pick out a scarf!");
+} else {
+	console.log("short sleeves are fine.");
+}
+
+console.log("now you are ready to go outside");
